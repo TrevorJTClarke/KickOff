9b4b6cc5bbd4b7790b307f8bab855f61ebc051ad
TrevorJTClarke <trevor@sevenly.org>
2013-11-25 22:13:06 -0800

    Initial Commit, setup all code to be generic, untested

---
+++ app.js
@@ -0,0 +1,28 @@
+var express  = require('express'),
+    async    = require("async"),
+    config   = require('./config'),
+	app      = express(),
+	server   = require('http').createServer(app);
+
+app.configure(function(){
+    app.set('views', __dirname + '/views');
+    app.set('view engine', 'ejs');
+    app.use(express.favicon(__dirname + '/favicon.ico'));
+	app.use(express.bodyParser());
+	app.use(express.cookieParser());
+	app.use(express.session({ secret: config.COOKIE_SECRET }));
+    app.use(express.static(__dirname + '/public'));
+});
+
+//TODO: change to load routes.js
+app.get('/', function(req, res){
+    res.render('index');
+});
+
+//start server
+server.listen(process.env.VCAP_APP_PORT || 9000, function(){
+    console.log('KickOff!');
+    console.log('http://localhost:9000');
+    console.log(' ');
+});
+
---
+++ config.js
@@ -0,0 +1,13 @@
+var config = {};
+
+//Storage Vars
+config.redis = {
+	PORT: 9999,
+	SERVER: "SERVER_URL",
+	SECRET: "SECRET_STRING"
+};
+
+//General Vars
+config.COOKIE_SECRET = "KickOff!";
+
+module.exports = config;
